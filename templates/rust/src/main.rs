07bc251b8d17c4ac3ec7f48aa8fd6e6f49e9167f
pblop <pablo@pabl.eu>
2025-12-01 13:18:31 +0100

    rust templates

---
+++ templates/rust/src/main.rs
@@ -0,0 +1,16 @@
+#[path = "../../../../../lib/rust.rs"]
+mod rust_runner;
+
+use rust_runner::runner;
+
+fn solve_pt1(input_data: &str, args: &[String]) -> i64 {
+    -1
+}
+
+fn solve_pt2(input_data: &str, args: &[String]) -> i64 {
+    -1
+}
+
+fn main() {
+    runner(solve_pt1, solve_pt2);
+}
